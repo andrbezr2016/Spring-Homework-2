3800fd91422bc31aa21d4605d0d0ef73898c8db6
Bezrukov Andrey <andrbezr2016@yandex.ru>
2022-01-01 22:37:11 +0300

    add swagger ui

--- src/main/java/com/andrbezr2016/springtask2/SpringBootStarter.java
+++ src/main/java/com/andrbezr2016/springtask2/SpringBootStarter.java
@@ -1,10 +1,14 @@
 package com.andrbezr2016.springtask2;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Bookstores API", version = "1.0", description = "Bookstores service"))
 public class SpringBootStarter {
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStarter.class, args);
     }
